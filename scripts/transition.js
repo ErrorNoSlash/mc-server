d3576ac33bd4ff0eefe4d95f72514b3f775cdbf5
ErrorNoSlash <187196647+ErrorNoSlash@users.noreply.github.com>
2026-01-05 16:47:13 +0100

    added transition

---
+++ scripts/transition.js
@@ -0,0 +1,63 @@
+/*controles transition*/
+document.addEventListener("DOMContentLoaded", () => {
+    const ease = "power4.inOut";
+
+    if (sessionStorage.getItem("transitioning") === "true") {
+        revealTransition().then(() => {
+            gsap.set(".block", { visibility: "hidden" });
+            sessionStorage.removeItem("transitioning");
+        });
+    } else {
+        gsap.set(".block", { visibility: "hidden" });
+    }
+
+    document.querySelectorAll("a").forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const href = link.getAttribute("href");
+
+            if (href && !href.startsWith("#") && href !== window.location.pathname) {
+                sessionStorage.setItem("transitioning", "true");
+                animateTransition().then(() => {
+                    window.location.href = href;
+                });
+            }
+        });
+    });
+
+    function revealTransition() {
+        return new Promise((resolve) => {
+            gsap.set(".block", { scaleY: 1 });
+            gsap.to(".block", {
+                scaleY: 0,
+                duration: 1,
+                stagger: {
+                    each: 0.1,
+                    from: "start",
+                    grid: "auto",
+                    axis: "x",
+                },
+                ease: ease,
+                onComplete: resolve,
+            });
+        });
+    }
+
+    function animateTransition() {
+        return new Promise((resolve) => {
+            gsap.set(".block", { visibility: "visible", scaleY: 0 });
+            gsap.to(".block", {
+                scaleY: 1,
+                duration: 1,
+                stagger: {
+                    each: 0.1,
+                    from: "start",
+                    grid: [2, 5],
+                    axis: "x",
+                },
+                ease: ease,
+                onComplete: resolve,
+            });
+        });
+    }
+});
